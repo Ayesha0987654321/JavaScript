c06eda28f4ef94b9380c85d7685e2c08d4d1eb5d
Ayesha0987654321 <javedayesha0987@gmail.com>
2025-07-02 00:26:04 -0700

    while and do while loops

---
+++ 05-loops-and-higher-order-arrays/03-do-while-loops/script.js
@@ -0,0 +1,44 @@
+// let i = 2;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+// const arr = [12, 32,45,16];
+// // let j = 0;
+// while(i < arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+
+
+// //Nesting the arrays
+// while (i <= 4){
+//     console.log(`Table of ${i}`);
+//     let j = 1 ; 
+//     while(j <= 10){
+//         console.log(`${i} x ${j} = ${i*j}`)
+//         j++;
+//     }
+//     console.log("--------------------------------------------------------")
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+// }while( i <= 5);
+
+
+
+// Fizz Buzz Chanllenge
+for(let i =2 ; i <= 100; i++){
+    if(i % 3 == 0){
+        console.log("Fizz");
+        continue;
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz");
+        continue;
+    }
+    console.log(i);
+}
